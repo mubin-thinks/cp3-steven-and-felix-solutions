40a6f695e11c9a31a2cac745fd1863355b9304df
mubin-thinks <mubin-thinks@proton.me>
2026-08-24 15:32:38 +0600

    added exercise_1-2-3_1.java, added .gitignore

---
+++ exercises/exercise_1-2-3/1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Program {
+        public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                double value = input.nextFloat();
+                System.out.printf("%7.3f\n", value);
+        }
+}
